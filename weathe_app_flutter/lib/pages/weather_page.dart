c8d1fb40d28d5188e848358eaed265ad260ec998
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-12-26 15:20:11 -0300

    pendente

--- weathe_app_flutter/lib/pages/weather_page.dart
+++ weathe_app_flutter/lib/pages/weather_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:weathe_app_flutter/models/weather_model.dart';
 import 'package:weathe_app_flutter/services/weather_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -34,33 +35,32 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
-    // animação
-    String getWeatherAnimation(String? mainCondition) {
-      if (mainCondition == null) return 'assets/sunny.json'; //default 
+  // animação
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json'; //default
 
-      switch (mainCondition.toLowerCase()) {
-        case 'clouds':
-        case 'mist':
-        case 'smoke':
-        case 'haze':
-        case 'dust':
-        case 'fog':
-          return 'assets/cloud.json';
-        case 'rain':
-        case 'drizzle':
-        case 'shower rain':
-          return 'assets/rain.json';
-        case 'thunderstorm':
-          return 'assets/thunder.json';
-        case 'clear':
-          return 'assets/sunny.json';
-        default: 
-          return 'assets/sunny.json';
-    
-      }
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
     }
+  }
 
-    // iniciar state
+  // iniciar state
   @override
   void initState() {
     super.initState();
@@ -71,22 +71,47 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // city name
-            Text(_weather?.cityName ?? 'loading city..'),
+      body: SafeArea(
+        minimum: EdgeInsets.all(80), // Padding
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // City Name
+              Column(
+                children: [
+                  Icon(
+                    Icons.place,
+                    size: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _weather?.cityName ?? "Loading city...",
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
 
-            // animation
-            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+              // Animation
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
 
-            // temperatura
-            Text('${_weather?.temperature.toString()}°C'),
-            
-            // weather condition 
-            Text(_weather?.mainCondition ?? ''),
-          ],
+              // Temperature
+              Column(
+                children: [
+                  Text(
+                    '${_weather?.temperature.round() ?? 'Loading temperature in '}°C',
+                    style: GoogleFonts.bebasNeue(fontSize: 50, height: 1),
+                  ),
+                  // Weather condition
+                  Text(_weather?.mainCondition ?? ""),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
